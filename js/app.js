b6edfc9f538923c68536f1af90055403298de8d1
Leo Fornasari <leofornasari@gmail.com>
2022-12-19 01:25:07 -0300

    Add existing file

---
+++ js/app.js
@@ -0,0 +1,169 @@
+Variables
+const marca = document.querySelector("#marca").value;
+const formulario = document.querySelector("#cotizar-seguro");
+const spinner = document.querySelector("#cargando")
+
+//Constructores
+function Seguro(marca, year, tipo){
+    this.marca = marca;
+    this.year = year;
+    this. tipo = tipo;
+}
+//Cotiza con los datos
+Seguro.prototype.cotizarSeguro = function(){
+    /*
+    1 = americano 1.15
+    2 = asiatico 1.05
+    3 = europeo 1.35
+    */ 
+
+    let cantidad;
+    const base = 2000;
+   
+   switch(this.marca){
+    case "1":
+        cantidad = base * 1.15;
+        break;
+    case "2":
+        cantidad = base * 1.05;
+        break;
+    case "3":
+        cantidad = base * 1.35;
+        break;
+    default:
+        break;
+   }
+
+   //Leer el año
+   const diferencia = new Date().getFullYear() - this.year;
+
+   cantidad -= ((diferencia *3) * cantidad ) / 100;
+
+   /*
+   si el seguro es basico se multiplica por 30%
+   si el seguro es completo se multiplica por 50%
+   */ 
+   if(this.tipo === "basico"){
+    cantidad *= 1.3;
+   }else{
+    cantidad *= 1.5;
+   }
+
+   return cantidad
+}
+
+function UI(){}
+
+UI.prototype.llenarOpciones = ()=>{
+    const max = new Date().getFullYear(),
+    min = max - 20;
+
+    const selectYear = document.querySelector("#year");
+
+    for(let i = max; i > min; i--){
+        let option = document.createElement("option");
+        option.value = i;
+        option.textContent = i
+        selectYear.appendChild(option);
+    }
+}
+
+//Muestra alerta en pantalla
+UI.prototype.mostrarMensaje = function(mensaje, tipo){
+    const div = document.createElement("div");
+    div.textContent = mensaje;
+    div.classList.add("mensaje", "mt-10");
+    if(tipo === "error"){
+        div.classList.add("error")
+    }else{
+        div.classList.add("correcto")
+    }
+    formulario.insertBefore(div, document.querySelector("#resultado"))
+
+    setTimeout(()=>{
+        div.remove();
+    }, 2000)
+}
+
+UI.prototype.mostrarResultado = function(seguro, total){
+
+    const {marca, year, tipo} = seguro;
+    let textoMarca;
+    switch(marca){
+        case "1":
+            textoMarca = "Americano"
+            break
+        case "2":
+            textoMarca = "Asiatico"
+            break
+        case "3":
+            textoMarca = "Europeo"
+            break
+        default:
+            break;
+    }
+
+    const div = document.createElement("div");
+    div.classList.add("mt-10");
+    div.innerHTML = `
+    <p class="header">Tu Resumen</p>
+    <p class="font-bold">Marca: <span class="font-normal">${textoMarca}</span></p>
+    <p class="font-bold">Año: <span class="font-normal">${year}</span></p>
+    <p class="font-bold">Tipo: <span class="font-normal capitalize">${tipo}</span></p>
+    <p class="font-bold">Total: <span class="font-normal">$${total}</span></p>
+    `;
+     const resultadoDiv = document.querySelector("#resultado");
+
+     
+     spinner.style.display = "block";
+     setTimeout(()=>{
+        spinner.style.display = "none";
+        resultadoDiv.appendChild(div); 
+     }, 2000)
+}
+
+const ui = new UI()
+
+//Event listeners
+document.addEventListener("DOMContentLoaded", ()=>{
+    ui.llenarOpciones();
+})
+
+eventListeners();
+function eventListeners(){
+    
+    formulario.addEventListener("submit", cotizarSeguro)
+}
+
+//Funciones
+function cotizarSeguro(e){
+    e.preventDefault();
+    
+    //Leer la marca
+    const marca = document.querySelector("#marca").value;
+
+    //Leer el año
+    const year = document.querySelector("#year").value;
+    //Leer el tipo de cobertura
+    const tipo = document.querySelector("input[name='tipo']:checked").value
+    
+    if(marca === "" || year === "" || tipo === ""){
+        ui.mostrarMensaje("Faltan datos", "error")
+        
+    }else{        
+        ui.mostrarMensaje("Cotizando")
+    }
+
+    // ocultar cotizacion previa
+    const resultados = document.querySelector("#resultado div");
+    if(resultados != null){
+        resultados.remove();
+    }
+    
+    const seguro = new Seguro(marca, year, tipo);
+    const total = seguro.cotizarSeguro(); 
+    
+    ui.mostrarResultado(seguro, total);
+
+}
+
